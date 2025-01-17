a67d81b2617cef871a1bfbcf5a4c6cc24483e394
sumikof <sumikof187@gmail.com>
2025-01-17 23:30:31 +0900

    install chromadb

---
+++ main.py
@@ -0,0 +1,22 @@
+import chromadb
+import pprint
+
+chroma_client = chromadb.Client()
+
+# switch `create_collection` to `get_or_create_collection` to avoid creating a new collection every time
+collection = chroma_client.get_or_create_collection(name="my_collection")
+
+# switch `add` to `upsert` to avoid adding the same documents every time
+collection.upsert(
+    documents=[
+        "This is a document about sports",
+        "This is a document about fruits"
+    ],
+    ids=["id1", "id2"]
+)
+results = collection.query(
+    query_texts=["This is a query document about oranges"], # Chroma will embed this for you
+    n_results=2 # how many results to return
+)
+
+pprint.pp(results)
